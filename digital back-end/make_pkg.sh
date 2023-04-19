3076a89923c98a7fca087513543e83f067a4d278
CarbonicKevin <kevin.kimminjun@gmail.com>
2023-04-18 21:53:25 -0400

    fixed some files

--- digital back-end/make_pkg.sh
+++ digital back-end/make_pkg.sh
@@ -2,4 +2,4 @@
 
 rm -rf build
 
-python3 setup.py $1 bdist bdist_wheel
+python3 setup.py custom bdist bdist_wheel
--- digital back-end/setup.py
+++ digital back-end/setup.py
@@ -78,7 +78,7 @@ copy_xrfclk()
 
 setup(
     name=package_name,
-    version='0.0.3',
+    version='0.1.0',
     install_requires=[ 'pynq==2.7', ],
     url='https://github.com/strath-sdr/rfsoc_sam',
     license='BSD 3-Clause License',
